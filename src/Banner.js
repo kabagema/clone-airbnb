fdc67462f270e8ba79e76d10fe1a923235620e9a
kabagema <35719135+kabagema@users.noreply.github.com>
2020-08-28 21:53:22 +0200

    airbnb app full and finished

---
+++ src/Banner.js
@@ -0,0 +1,38 @@
+import React, { useState }from 'react'
+import './Banner.css'
+import { Button } from '@material-ui/core'
+import Search from './Search'
+import { useHistory } from 'react-router-dom'
+function Banner() {
+
+    const history = useHistory(); //browser keeps history of where you at
+    const [showSearch, setShowSearch] = useState(false);
+    return (
+        <div className='banner'>
+            <div className = 'banner__search'>
+                {showSearch && <Search />}
+
+            <Button onClick={() => setShowSearch(!showSearch)}
+            className='banner__searchbutton'
+            variant='outlined'>
+                {showSearch ? "Hide" : "Search Dates"}
+               
+            </Button>
+            </div>
+            <div className='banner_info'>
+                <h1>
+                    Get out and stretch your imagination
+                </h1>
+                <h5>
+                    Plan a different kind of gateway to uncover the hidden gems near you
+                </h5>
+                <Button onClick={() => history.push('/search')}
+                variant='outlined'>
+                    Explore Nearby
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
